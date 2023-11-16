77c4e5f8bfaef219a6cce4cb865edd14482159f8
Sabrinavf <contactsabrinafernandes@gmail.com>
2023-11-16 13:24:23 -0300

    Adicionar arquivos de projeto.

---
+++ Split2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' '); // vai ler os dados guardar na variável 'vet' e recortar com o comando split
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);       // cada palavra 'recortada' salva em um vetor de string
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture)); // saída de dados
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+        }
+
+    }
+}
